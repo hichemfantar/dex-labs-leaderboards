3ac1fd834ec13d267304d373a340bb777775bbf4
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-11 23:06:12 +0100

    improve header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,36 @@
 import { useIsFetching } from "react-query";
+import appLogo from "assets/images/logos/logo48.png";
 
 export default function Header() {
 	const isFetching = useIsFetching();
 
 	return (
 		<div className="c-header">
-			{/* <img
-      className="c-logo"
-      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/813538/km-logo-color.svg"
-      draggable="false"
-    /> */}
 			<button className="c-button c-button--primary">
 				DexLabs Leaderboards
 			</button>
-			<h3
+			<div
 				style={{
-					opacity: isFetching ? 1 : 0,
+					display: "flex",
+					gap: 10,
+					justifyContent: "center",
+					alignItems: "center",
 				}}
 			>
-				fetching
-			</h3>
+				<h3
+					style={{
+						opacity: isFetching ? 1 : 0,
+					}}
+				>
+					fetching
+				</h3>
+				<img
+					className="c-logo"
+					src={appLogo}
+					draggable="false"
+					alt="app logo"
+				/>
+			</div>
 		</div>
 	);
 }
