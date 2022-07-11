a3a7ebf4006427723abd4d9dd880c5a0c8e7fe59
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-11 19:00:03 +0100

    prettier server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -14,4 +15,5 @@ app.get("/ping", function (req, res) {
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 app.listen(port);
