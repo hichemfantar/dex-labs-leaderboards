4fc47b12ce8ebf6bdd5571397d8c125ebf684eac
Hichem Fantar <hichemfantar2049@gmail.com>
2022-07-12 00:48:53 +0100

    load fonts from css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import appConstants from "appConstants";
 import axios from "axios";
-import "./App.css";
+import "./assets/css/App.css";
 import Leaderboard from "./components/Leaderboard";
 
 // only set baseurl in prod because dev is using proxy
